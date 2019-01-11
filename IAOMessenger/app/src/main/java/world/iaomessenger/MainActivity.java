e8c5e01feb126aab4698819514d08cc37d43c43d
Badr Benchagra <badr.redax@gmail.com>
2019-01-11 19:57:01 +0100

    First Commit lele xo

--- IAOMessenger/app/src/main/java/world/iaomessenger/MainActivity.java
+++ IAOMessenger/app/src/main/java/world/iaomessenger/MainActivity.java
@@ -2,12 +2,39 @@ package world.iaomessenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar main_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //TODO: do stuff here with items later!
+        return super.onOptionsItemSelected(item);
     }
 }
