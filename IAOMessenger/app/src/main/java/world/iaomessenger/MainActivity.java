edf954b26175e339b1f5a25fc08328ed53a0d03b
Badr Benchagra <badr.redax@gmail.com>
2019-01-11 19:58:41 +0100

    First Commit lele xo

--- IAOMessenger/app/src/main/java/world/iaomessenger/MainActivity.java
+++ IAOMessenger/app/src/main/java/world/iaomessenger/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar main_toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
